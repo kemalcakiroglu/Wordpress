730b299f47e1799fd30376e43c79af65a808ba15
kemalcakiroglu <51874659+kemalcakiroglu@users.noreply.github.com>
2019-12-09 20:54:25 +0300

    2.deneme

--- wp-login.php
+++ wp-login.php
@@ -9,11 +9,11 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<title>Giriş &lsaquo; Kemal Çakıroğlu &#8212; WordPress</title>
 	<link rel='dns-prefetch' href='//s.w.org' />
-<link rel='stylesheet' id='dashicons-css'  href='/https://kemalcakiroglu.github.io/Wordpress/wp-includes/css/dashicons.min.css?ver=5.3' media='all' />
-<link rel='stylesheet' id='buttons-css'  href='/https://kemalcakiroglu.github.io/Wordpress/wp-includes/css/buttons.min.css?ver=5.3' media='all' />
-<link rel='stylesheet' id='forms-css'  href='/https://kemalcakiroglu.github.io/Wordpress/wp-admin/css/forms.min.css?ver=5.3' media='all' />
-<link rel='stylesheet' id='l10n-css'  href='/https://kemalcakiroglu.github.io/Wordpress/wp-admin/css/l10n.min.css?ver=5.3' media='all' />
-<link rel='stylesheet' id='login-css'  href='/https://kemalcakiroglu.github.io/Wordpress/wp-admin/css/login.min.css?ver=5.3' media='all' />
+<link rel='stylesheet' id='dashicons-css'  href='http://localhost/https:/kemalcakiroglu.github.io/Wordpress/wp-includes/css/dashicons.min.css?ver=5.3' media='all' />
+<link rel='stylesheet' id='buttons-css'  href='http://localhost/https:/kemalcakiroglu.github.io/Wordpress/wp-includes/css/buttons.min.css?ver=5.3' media='all' />
+<link rel='stylesheet' id='forms-css'  href='http://localhost/https:/kemalcakiroglu.github.io/Wordpress/wp-admin/css/forms.min.css?ver=5.3' media='all' />
+<link rel='stylesheet' id='l10n-css'  href='http://localhost/https:/kemalcakiroglu.github.io/Wordpress/wp-admin/css/l10n.min.css?ver=5.3' media='all' />
+<link rel='stylesheet' id='login-css'  href='http://localhost/https:/kemalcakiroglu.github.io/Wordpress/wp-admin/css/login.min.css?ver=5.3' media='all' />
 	<meta name='robots' content='noindex,noarchive' />
 	<meta name='referrer' content='strict-origin-when-cross-origin' />
 		<meta name="viewport" content="width=device-width" />
@@ -25,7 +25,7 @@
 		<div id="login">
 		<h1><a href="https://wordpress.org/">WordPress'in desteğiyle</a></h1>
 	
-		<form name="loginform" id="loginform" action="/https://kemalcakiroglu.github.io/Wordpress/wp-login.php" method="post">
+		<form name="loginform" id="loginform" action="http://localhost/https:/kemalcakiroglu.github.io/Wordpress/wp-login.php" method="post">
 			<p>
 				<label for="user_login">Kullanıcı adı ya da e-posta adresi</label>
 				<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off" />
@@ -49,34 +49,34 @@
 		</form>
 
 					<p id="nav">
-									<a href="/https://kemalcakiroglu.github.io/Wordpress/wp-login.php?action=lostpassword">Parolanızı mı unuttunuz?</a>
+									<a href="http://localhost/https:/kemalcakiroglu.github.io/Wordpress/wp-login.php?action=lostpassword">Parolanızı mı unuttunuz?</a>
 								</p>
 					<script type="text/javascript">
 			function wp_attempt_focus() {setTimeout( function() {try {d = document.getElementById( "user_login" );d.focus(); d.select();} catch( er ) {}}, 200);}
 wp_attempt_focus();
 if ( typeof wpOnload === 'function' ) { wpOnload() }		</script>
-				<p id="backtoblog"><a href="/https://kemalcakiroglu.github.io/Wordpress/">
+				<p id="backtoblog"><a href="http://localhost/https:/kemalcakiroglu.github.io/Wordpress/">
 		&larr; Kemal Çakıroğlu sitesine geri dön		</a></p>
 			</div>
-	<script src='/https://kemalcakiroglu.github.io/Wordpress/wp-includes/js/jquery/jquery.js?ver=1.12.4-wp'></script>
-<script src='/https://kemalcakiroglu.github.io/Wordpress/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.4.1'></script>
+	<script src='http://localhost/https:/kemalcakiroglu.github.io/Wordpress/wp-includes/js/jquery/jquery.js?ver=1.12.4-wp'></script>
+<script src='http://localhost/https:/kemalcakiroglu.github.io/Wordpress/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.4.1'></script>
 <script>
 var _zxcvbnSettings = {"src":"\/wp-includes\/js\/zxcvbn.min.js"};
 </script>
-<script src='/https://kemalcakiroglu.github.io/Wordpress/wp-includes/js/zxcvbn-async.min.js?ver=1.0'></script>
+<script src='http://localhost/https:/kemalcakiroglu.github.io/Wordpress/wp-includes/js/zxcvbn-async.min.js?ver=1.0'></script>
 <script>
 var pwsL10n = {"unknown":"Parola g\u00fcc\u00fc bilinmiyor","short":"\u00c7ok zay\u0131f","bad":"Zay\u0131f","good":"Orta","strong":"G\u00fc\u00e7l\u00fc","mismatch":"Parolalar uyu\u015fmuyor"};
 </script>
-<script src='/https://kemalcakiroglu.github.io/Wordpress/wp-admin/js/password-strength-meter.min.js?ver=5.3'></script>
-<script src='/https://kemalcakiroglu.github.io/Wordpress/wp-includes/js/underscore.min.js?ver=1.8.3'></script>
+<script src='http://localhost/https:/kemalcakiroglu.github.io/Wordpress/wp-admin/js/password-strength-meter.min.js?ver=5.3'></script>
+<script src='http://localhost/https:/kemalcakiroglu.github.io/Wordpress/wp-includes/js/underscore.min.js?ver=1.8.3'></script>
 <script>
 var _wpUtilSettings = {"ajax":{"url":"\/Wordpress\/wp-admin\/admin-ajax.php"}};
 </script>
-<script src='/https://kemalcakiroglu.github.io/Wordpress/wp-includes/js/wp-util.min.js?ver=5.3'></script>
+<script src='http://localhost/https:/kemalcakiroglu.github.io/Wordpress/wp-includes/js/wp-util.min.js?ver=5.3'></script>
 <script>
 var userProfileL10n = {"warn":"Yeni parolan\u0131z kaydedilemedi.","warnWeak":"Zay\u0131f parola kullanmay\u0131 onayla","show":"G\u00f6ster","hide":"Gizle","cancel":"Vazge\u00e7","ariaShow":"Parolay\u0131 g\u00f6ster","ariaHide":"Parolay\u0131 gizle"};
 </script>
-<script src='/https://kemalcakiroglu.github.io/Wordpress/wp-admin/js/user-profile.min.js?ver=5.3'></script>
+<script src='http://localhost/https:/kemalcakiroglu.github.io/Wordpress/wp-admin/js/user-profile.min.js?ver=5.3'></script>
 	<script>
 	/(trident|msie)/i.test(navigator.userAgent)&&document.getElementById&&window.addEventListener&&window.addEventListener("hashchange",function(){var t,e=location.hash.substring(1);/^[A-z0-9_-]+$/.test(e)&&(t=document.getElementById(e))&&(/^(?:a|select|input|button|textarea)$/i.test(t.tagName)||(t.tabIndex=-1),t.focus())},!1);
 	</script>
